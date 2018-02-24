02488eeeecf29f5508b8da07707cae67fc8a766d
Paulo Conde <pauloconde@gmail.com>
2018-02-24 17:02:57 -0400

    Delete to initial state

--- pin.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost", "descuent_admin", "vitacodeDB", "descuent_vitacode");
-$resultado = mysqli_query($mysqli, "SELECT ID AS _id, PIN as _pin FROM PARES WHERE ID='02FAF086'");
-$fila = mysqli_fetch_assoc($resultado);
-echo $fila['_id'];
-echo $fila['_pin'];
-?>
